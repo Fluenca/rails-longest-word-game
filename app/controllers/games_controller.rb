cd33190e414e4771d5b35f4b3790c719ff08d3e0
Philipp Krink <krink.philipp@gmail.com>
2021-11-08 17:19:58 +0100

    Seems to work now

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,10 +10,12 @@ class GamesController < ApplicationController
     @response = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}").read
     @parsed = JSON.parse(@response)
       case
-      when params[:game].split.include?(params[:word].split) != true
+      when params[:word].split(//).to_set.subset?(params[:game].split(//).to_set) != true
         @wrong = 'Sorry your word can not be made out of the letters.'
       when @parsed['found'] == false
         @wrong = 'Sorry your word does not seem to exist in the english language.'
+      else
+        @wrong = 'Congrats your word is a valid english word!'
       end
   end
 end
